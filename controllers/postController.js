9a60cdbf1c4693a56ced271b44a0f8a59b2798b0
sinhaji2000 <sinhaoffcial@gmail.com>
2024-09-23 18:23:11 +0530

    delete post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const Post = require("../models/post");
-
+const Comment = require("../models/comment");
 exports.postPost = async (req, res) => {
   try {
     const post = await Post.create({
@@ -11,5 +11,34 @@ exports.postPost = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).send("Error Posting ");
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).send("Post not found");
+    }
+
+    console.log("Post User ID: ", post.user.toString());
+    console.log("Logged-in User ID: ", req.user.id.toString());
+
+    // Check if the user deleting the post is the owner
+    if (post.user.toString() === req.user.id.toString()) {
+      // Use findByIdAndDelete instead of post.remove()
+      await Post.findByIdAndDelete(req.params.id);
+
+      // Remove all comments related to this post
+      await Comment.deleteMany({ post: req.params.id });
+
+      return res.redirect("/");
+    } else {
+      return res.status(403).send("Unauthorized action");
+    }
+  } catch (error) {
+    console.error("Error while deleting post:", error);
+    return res.status(500).send("Error while Deleting Post");
   }
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const Router = express.Router();
 const postController = require("../controllers/postController");
+const passport = require("../config/passport-local-strategy");
 
 Router.post("/create-post", postController.postPost);
+Router.post(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  postController.deletePost
+);
 
 module.exports = Router;
